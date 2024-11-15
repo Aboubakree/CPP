c436b607679638f5048f985b5e6795911db93c6c
Aboubakr Krid <akrid@c3r2p3.1337.ma>
2024-11-15 22:09:46 +0100

    ex00 almost done

---
+++ cpp01/ex00/Zombie.cpp
@@ -0,0 +1,23 @@
+#include "Zombie.hpp"
+
+
+Zombie::Zombie()
+{}
+
+Zombie::~Zombie()
+{}
+
+void Zombie::announce(void)
+{
+    std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+void Zombie::set_name(std::string name)
+{
+    this->name = name;
+}
+
+std::string Zombie::get_name()
+{
+    return this->name ;
+}
---
+++ cpp01/ex00/Zombie.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+
+class  Zombie
+{
+    private:
+        std::string name;
+    public:
+        Zombie();
+        ~Zombie();
+        void set_name(std::string name);
+        std::string get_name();
+        void announce(void);
+};
+
+Zombie* newZombie( std::string name );
+void    randomChump( std::string name );
