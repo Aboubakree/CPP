e382fa01b84a3b03cd01acc505ba5184ac0696ef
Aboubakr Krid <akrid@c3r4p1.1337.ma>
2025-08-03 16:04:21 +0100

    working on 08

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+template <typename T>
+class MutantStack
+{
+private:
+    
+public:
+    MutantStack();
+    ~MutantStack();
+};
+
+template <typename T>
+MutantStack::MutantStack()
+{
+}
+
+template <typename T>
+MutantStack::~MutantStack()
+{
+}
+
+#include "MutantStack.tpp"
